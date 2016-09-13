0fb4de0cea20c9cc7527cb1bffe5da80db96348b
Nook Harquail <nook@yieldmo.com>
2016-09-13 15:06:06 -0400

    minor comments

--- Uncrustify/SourceEditorCommand.swift
+++ Uncrustify/SourceEditorCommand.swift
@@ -26,12 +26,15 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         task.standardInput = invocation.buffer.completeBuffer
         task.standardOutput = outputPipe
         task.launchPath = commandPath
+        
+        // configure uncrustify to format with bundled uncrustify.cfg, format for Objective-C, and strip messages
         task.arguments = [ "-c=\(commandConfigPath)","-l=OC","-q"]
         
         let inputPipe = Pipe()
         task.standardInput = inputPipe
         let stdinHandle = inputPipe.fileHandleForWriting
         
+        // write text to stdin (where uncrustify reads from)
         if let data = invocation.buffer.completeBuffer.data(using: .utf8) {
             stdinHandle.write(data)
             stdinHandle.closeFile()
@@ -51,6 +54,7 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             
         }
         
+        // fixes crash if there is no selection when completion handler is called
         invocation.buffer.selections.add(XCSourceTextRange(start: XCSourceTextPosition(line: 0, column: 0), end: XCSourceTextPosition(line: 0, column: 0)))
         
         completionHandler(nil)
